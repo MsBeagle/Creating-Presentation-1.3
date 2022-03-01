fc01349664965f2f9969176fdc16513cfca9c040
MsBeagle <aga.ihnatowicz@gmail.com>
2022-03-01 18:22:33 +0100

    training

---
+++ story_content/5nXQj4DMdkr_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.44.23192.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:02.280%0D%0AUse%20no%20more%20than%20four%20colors%20on%20one%20chart.%0D%0A%0D%0A"}')
---
+++ story_content/6ZrAkBbs1f7_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.44.23192.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.824%0D%0ALimit%20the%20information%20to%20essentials.%0D%0A%0D%0A00:00:02.149%20--%3E%2000:00:03.816%0D%0AContent%20should%20be%20self-evident.%0D%0A%0D%0A"}')
